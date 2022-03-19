a4b9178dcbb62177f3515fa05b393f7f3f43ba8f
nanreh1976 <hernanchogomez@gmail.com>
2022-03-19 20:13:04 -0300

    agregando EmpleadoComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-raiz',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = '1rapp';
+
+   saludo="Hola alumnos de angular, que tal estan?"
 }
