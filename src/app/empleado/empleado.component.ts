df3dad58711129eb05d277dba16dcf4fd87fe1f6
nanreh1976 <hernanchogomez@gmail.com>
2022-04-04 21:03:10 -0300

    manejo de componentes , creacion con css o html

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -2,8 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-empleado',
-  templateUrl: './empleado.component.html',
-  styleUrls: ['./empleado.component.css']
+   templateUrl: './empleado.component.html',
+  //template:"<p>Aqui iria un empleado</p>",
+  
+   styleUrls: ['./empleado.component.css']
+  //styles:["p{background-color:red;}"]
 })
 export class EmpleadoComponent implements OnInit {
 
